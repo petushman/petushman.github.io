dfb108d2433624098257970d7233d6fea0daa7bd
Mo4a4os <valievgayaz2009@gmail.com>
2026-01-11 14:39:58 +0300

    added 1-4

--- src/pages/Izd.jsx
+++ src/pages/Izd.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import { BookOpen } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Izd = () => {
   return (
-    <div>Izd</div>
-  )
-}
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Категория</th>
+              <th>Название</th>
+              <th>Дата создания</th>
+              <th>Рейтинг</th>
+              <th>Оценка</th>
+              <th>Полный текст / видео</th>
+              <th>Ссылка на статью</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Старые хроники</td>
+              <td>Раиль и Аяз</td>
+              <td>2019</td>
+              <td>18+</td>
+              <td></td>
+              <td>
+                <Link className="btn ml-10 btn-primary" to="/texts/one">
+                  <BookOpen />
+                </Link>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>Старые хроники</td>
+              <td>Девочки 11-13 лет</td>
+              <td>2021</td>
+              <td>16+</td>
+              <td></td>
+              <td>
+                <Link className="btn ml-10 btn-primary" to="/texts/two">
+                  <BookOpen />
+                </Link>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Старые хроники</td>
+              <td>Мальчики 11-13 лет (Фанаты Девочек 11-13 лет)</td>
+              <td>2021</td>
+              <td>18+</td>
+              <td></td>
+              <td>
+                <Link className="btn ml-10 btn-primary" to="/texts/three">
+                  <BookOpen />
+                </Link>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+        <th>4</th>
+        <td>Старые хроники</td>
+        <td>Мальчики 11-13 лет (Банька)  </td>
+        <td>2021</td>
+        <td>18+</td>
+        <td></td>
+        <td><Link className='btn ml-10 btn-primary' to='/texts/four'><BookOpen /></Link></td>
+        <td></td>
+      </tr>
+            {/*
+      <tr>
+        <th></th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><Link className='btn ml-10 btn-primary' to='/texts/'><BookOpen /></Link></td>
+        <td></td>
+      </tr>
+      */}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Izd
+export default Izd;
--- src/pages/Routers.jsx
+++ src/pages/Routers.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import History from './History'
-import Izd from './Izd'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import History from "./History";
+import Izd from "./Izd";
+import One from "./texts/one";
+import Two from "./texts/Two";
+import Three from "./texts/Three";
+import Four from "./texts/Four";
+
 
 const Routers = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path='/history' element={<History />}></Route>
-        <Route path="/izd" element={<Izd />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/history" element={<History />}></Route>
+      <Route path="/izd" element={<Izd />}></Route>
+      <Route path="/texts/one" element={<One />}></Route>
+      <Route path="/texts/two" element={<Two />}></Route>
+      <Route path="/texts/Three" element={<Three />}></Route>
+      <Route path="/texts/Four" element={<Four />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;
